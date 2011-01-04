f0d448ccb117320217857b14210f4d0a3c82350e
David M Peixotto <dmp@rice.edu>
2011-01-04 13:51:22 -0600

    Make trace tool thread safe
    We now output basic block traces to one file per thread.

--- trace/run.sh
+++ trace/run.sh
@@ -5,6 +5,8 @@ OBJDIR=obj-ia32
 echo == STARTING ==
 $PIN -t $OBJDIR/trace.so -- ./test &
 PID=$!
+#top -H -p $PID
+#strace -f -e trace=clone -p $PID
 wait
 echo == FINISHED ==
-echo See output in file "trace.out"
+echo See output in file trace.out.*
